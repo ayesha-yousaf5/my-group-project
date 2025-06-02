8bed10aa273ecbb1615d97156a1e75104c04fc45
Ayesha Yousaf <aishayousuf164@gmail.com>
2025-06-03 00:10:02 +0500

    Update.php

--- Update.php
+++ Update.php
@@ -1,31 +1,89 @@
+<?php 
+include "config.php";
+
+if (isset($_POST['update'])) {
+    $user_id = $_POST['user_id'];
+    $fullName = $_POST['fullName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+
+    $sql = "UPDATE shippinginfo SET 
+                fullname = '$fullName',
+                email = '$email',
+                phone = '$phone',
+                address = '$address',
+                city = '$city'
+            WHERE id = '$user_id'";
+
+    $result = $conn->query($sql);
+
+    if ($result === TRUE) {
+        echo "Information record updated successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} 
+
+if (isset($_GET['id'])) {
+    $user_id = $_GET['id']; 
+
+    $sql = "SELECT * FROM shippinginfo WHERE id = '$user_id'";
+    $result = $conn->query($sql); 
+
+    if ($result->num_rows > 0) {        
+        while ($row = $result->fetch_assoc()) {
+            $fullName = $row['fullName'];
+            $email = $row['email'];
+            $phone = $row['phone'];
+            $address = $row['address'];
+            $city = $row['city'];
+            $id = $row['id'];
+        } 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Shipping Info</title>
+</head>
+<body>
+
+<h2>Shipping Information Update Form</h2>
+
+<form action="" method="post">
+  <fieldset>
+    <legend>Update Details:</legend>
+
+    Full Name:<br>
+    <input type="text" name="fullName" value="<?php echo $fullName; ?>"><br>
+
+    <input type="hidden" name="user_id" value="<?php echo $id; ?>">
+
+    Email:<br>
+    <input type="email" name="email" value="<?php echo $email; ?>"><br>
+
+    Phone:<br>
+    <input type="text" name="phone" value="<?php echo $phone; ?>"><br>
+
+    Address:<br>
+    <input type="text" name="address" value="<?php echo $address; ?>"><br>
+
+    City:<br>
+    <input type="text" name="city" value="<?php echo $city; ?>"><br><br>
+
+    <input type="submit" value="Update" name="update">
+
+  </fieldset>
+</form>
+
+</body>
+</html>
+
 <?php
-include 'Config.php';
-// Get POSTed form data
-$id       = intval($_POST['id']);
-$fullName = $conn->real_escape_string($_POST['fullName']);
-$email    = $conn->real_escape_string($_POST['email']);
-$phone    = $conn->real_escape_string($_POST['phone']);
-$address  = $conn->real_escape_string($_POST['address']);
-$city     = $conn->real_escape_string($_POST['city']);
-
-// Build UPDATE query
-$sql = "
-    UPDATE shippinginfo SET 
-      fullName = '$fullName',
-      email    = '$email',
-      phone    = '$phone',
-      address  = '$address',
-      city     = '$city'
-    WHERE id = $id
-";
-
-if ($conn->query($sql) === TRUE) {
-    // Redirect back to view.php after successful update
-    header("Location: view.php");
-    exit();
-} else {
-    echo "Error updating record: " . $conn->error;
+    } else { 
+        header('Location: view.php');
+    } 
 }
-
-$conn->close();
 ?>
